aaa0152d4855ddfc1d785f7127a9f10fb5ff1d91
JaksonFives <jpv.lic16@uea.edu.br>
2019-07-08 14:32:51 -0400

    Teste de vídeo com Modal

--- js/funcoes_video.js
+++ js/funcoes_video.js
@@ -1,33 +1,21 @@
 var ativo = false;
 
-function openFullscreen(elem,btn_play,btn_pause) {
+function playPause(elem,btn_play,btn_pause) {
     if(ativo==false){
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-            elem.play();
-            ativo = true;
-            btn_play.style.display = "none";
-            btn_pause.style.display = "initial";
-        } else if (elem.mozRequestFullScreen) { /* Firefox */
-            elem.mozRequestFullScreen();
-            elem.play();
-            ativo = true;
-            btn_play.style.display = "none";
-            btn_pause.style.display = "initial";
-        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) { /* IE/Edge */
-            elem.msRequestFullscreen();
-            elem.play();
-            ativo = true;
-            btn_play.style.display = "none";
-            btn_pause.style.display = "initial";
-        }
+        elem.play();
+        ativo = true;
+        /*btn_play.style.display = "none";
+        btn_pause.style.display = "initial";*/
     }else{
         elem.pause();
-        btn_play.style.display = "initial";
-        btn_pause.style.display = "none";
+        /*btn_play.style.display = "initial";
+        btn_pause.style.display = "none";*/
         ativo = false;
     }
     
+}
+
+function stopVideo(elem){
+    elem.pause();
+    elem.currentTime = 0;
 }
